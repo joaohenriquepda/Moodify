5b816ccc113764f226879d4ca21ff4308d8842af
phe0 <pheofo@gmail.com>
2019-06-28 15:35:33 -0300

    Change script so that it return a list of audio_features when given a playlist URI

--- sp_searcher.py
+++ sp_searcher.py
@@ -7,18 +7,34 @@ from spotipy.oauth2 import SpotifyClientCredentials
 CLIENT_ID = os.getenv('CLIENT_ID', '')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET', '')
 
-playlist_id = sys.argv[1] # Argument passed on command line
+# playlist_id = sys.argv[1] # Argument passed on command line
 
-file_path = sys.argv[2] # File path to save results
+# file_path = sys.argv[2] # File path to save results
 
 client_credentials_manager = SpotifyClientCredentials(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET
 )
+
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-playlist = sp.user_playlist('Spotify', playlist_id=playlist_id)
+searchQuery = input(">>> Insert the URI of the playlist: ").split(":")
 
-with open(file_path, 'w') as f:
-    json.dump(playlist['tracks'], f, indent=2)
-    print('FINISH')
+searchResults = sp.user_playlist_tracks(searchQuery[2], searchQuery[4], fields='items')
+print()
+
+
+ids = []
+
+for item in searchResults["items"]:
+    ids.append(item["track"]["id"])
+
+
+features = sp.audio_features(ids)
+
+print(json.dumps(features, sort_keys=True, indent=4))
+
+
+# with open(file_path, 'w') as f:
+#     json.dump(playlist['tracks'], f, indent=2)
+#     print('FINISH')
