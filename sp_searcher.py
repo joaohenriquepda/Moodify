82e5acb9979454d6fcb86887c2dfdfe582474b72
phe0 <pheofo@gmail.com>
2019-06-28 16:01:31 -0300

    Change script to receive many URIs

--- sp_searcher.py
+++ sp_searcher.py
@@ -7,34 +7,36 @@ from spotipy.oauth2 import SpotifyClientCredentials
 CLIENT_ID = os.getenv('CLIENT_ID', '')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET', '')
 
-# playlist_id = sys.argv[1] # Argument passed on command line
-
-# file_path = sys.argv[2] # File path to save results
+file_path = sys.argv[1] # File path to save results
 
 client_credentials_manager = SpotifyClientCredentials(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET
 )
 
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-searchQuery = input(">>> Insert the URI of the playlist: ").split(":")
-
-searchResults = sp.user_playlist_tracks(searchQuery[2], searchQuery[4], fields='items')
-print()
-
-
 ids = []
+features = []
 
-for item in searchResults["items"]:
-    ids.append(item["track"]["id"])
+while True:
+    searchQuery = input(">>> Insira o URI da playlist ou 0 para parar a aplicação: ").split(":")
+    print(searchQuery)
+
+    if searchQuery[0] == "0":
+        break
+
+    else:
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+        searchResults = sp.user_playlist_tracks(searchQuery[2], searchQuery[4], fields='items')
+        print()
+
+        for item in searchResults["items"]:
+            ids.append(item["track"]["id"])
+
+for id in ids:
+    features.append(sp.audio_features(id))
 
 
-features = sp.audio_features(ids)
-
-print(json.dumps(features, sort_keys=True, indent=4))
-
-
-# with open(file_path, 'w') as f:
-#     json.dump(playlist['tracks'], f, indent=2)
-#     print('FINISH')
+with open(file_path, 'w') as f:
+    json.dump(features, f, indent=2)
+    print('FINISH')
